5977bbacef0e2ed9257ff02820150ee1983a0f40
garretthilberling <ghilberling@knights.ucf.edu>
2022-05-02 19:57:05 -0400

    finished tests for nav component

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
-const { asFragment } = render(<About />);
 
 afterEach(cleanup);
 
@@ -11,10 +10,10 @@ describe('About component', () => {
     it('renders', () => {
         render(<About />);
     });
-
+    
     // test 2
     it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
         expect(asFragment()).toMatchSnapshot();
     });
-
 });
